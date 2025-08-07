c022c5ecc6d57599ec759f5981628d2dec0b5681
NTISHOR <augustineakpotu5@gmail.com>
2025-08-07 16:33:35 +0100

    Commit3

--- script.js
+++ script.js
@@ -2,29 +2,21 @@ const { createRoot } = ReactDOM;
 const root = createRoot(document.getElementById('root'));
 
 function CounterApp() {
-  const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+    const operations = [
+        { label: 'Increment', func: () => setCount(count + 1) },
+        { label: 'Reset', func: () => setCount(0) },
+        { label: 'Decrement', func: () => setCount(count - 1) },
+    ];
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const reset = () => {
-    setCount(0);
-  };
-
-  return React.createElement(
-    'div',
-    null,
-    React.createElement('h1', null, 'Counter App'),
-    React.createElement('h2', null, count),
-    React.createElement('button', { onClick: increment }, 'Increment'),
-    React.createElement('button', { onClick: reset }, 'Reset'),
-    React.createElement('button', { onClick: decrement }, 'Decrement')
-  );
+    return React.createElement(
+        'div',
+        null,
+        React.createElement('h1', null, 'Counter App'),
+        React.createElement('h2', null, count),
+        operations.map((op, index) =>
+            React.createElement('button', { key: index, onClick: op.func }, op.label)
+        )
+    );
 }
-
-root.render(React.createElement(CounterApp));
